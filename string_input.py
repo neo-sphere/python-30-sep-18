7c2b1d1cb64e002b7fdf3c0a862a43bd231a5a40
Santosh Purbey <santosh.purbey@logpoint.com>
2018-10-09 19:12:56 +0545

    String Input Output Operations

---
+++ string_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+# print('type of name', type(name))
+# print('type of age', type(age))
+# print('my name is ', name)
+# print('My age is ', age)
+# print('My name is %s and age is %s' %(name, age))
+# print('my name is: {}, age: {}'.format(name, age))
+# print('my name is: {1}, age: {0}'.format(age, name))
+# print('my name is: {1}, age: {0}'.format(name, age))
+# print('my name is: {n}, age: {a}'.format(n = name, a = age))
+# print('my name is: {name}, age: {age}'.format(age = age, name=name))
+print(f'my name is {name} and age is {age}')
