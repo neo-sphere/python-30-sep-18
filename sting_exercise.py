0259d0a016671fbf24071ece633bd0e7b17fddba
Santosh Purbey <santosh.purbey@logpoint.com>
2018-10-14 15:42:46 +0545

    string class test exercise
    this exercise to test students understanding about string manuplationa and method to apply knowledge in real world problem

---
+++ sting_exercise.py
@@ -0,0 +1,8 @@
+# write a program to count number of character in a string except whitespace!
+# write a program to count number special character in a string except whitespace!
+my_string = "I l@v# pyt$on!"
+# new = my_string.split(' ')
+print(len(''.join(my_string.split())))
+# print(len(my_string))
+# print(my_string.count(' '))
+# print(len(my_string)- my_string.count(' '))
