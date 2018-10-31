656ebeef4573718dc640906d46f75d5e334d9247
Santosh Purbey <santosh.purbey@logpoint.com>
2018-10-31 17:45:28 +0545

    for else and if else

---
+++ for_else.py
@@ -0,0 +1,12 @@
+# forloop execute without break
+# for i in range(10):
+#     print(i)
+# else:
+#     print('Done!')
+# forloop with break
+for i in range(20):
+    if i >= 15:
+        continue
+    print(i)
+else:
+    print('Done!!!')
---
+++ if_elif_else.py
@@ -0,0 +1,22 @@
+# age = input('Enter your age:') # input datatype str
+# age = int(age) # datatype conversion
+# age = int(input('Enter your age:')) # input datatype str
+# if age < 0 or age > 120:
+#     print('Invalid Input!')
+# elif age <= 2:
+#     print('Infant!')
+# elif age <= 12:
+#     print('Child!')
+# elif age <= 19:
+#     print('Teenager!')
+# elif age <= 40:
+#     print('Adult!')
+# else:
+#     print('your are old')
+#
+#  Ternary operator
+obtain_marks = 34
+is_pass = True if obtain_marks > 40 else False
+print(is_pass)
+is_pass = 'Pass' if obtain_marks > 40 else 'Fail'
+print(is_pass)
